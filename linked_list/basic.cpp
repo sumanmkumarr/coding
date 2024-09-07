42158cd3bf3cbfbe5e9b2524badd786bccb05a66
suman kumar <sumang91222@gmail.com>
2024-09-08 02:17:46 +0530

    traversal

---
+++ linked_list/basic.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+   int data;
+   node* next;
+
+   node(int data1,node* next1)
+   {
+     data=data1;
+     next=next1;
+   }
+};
+
+node* convertarr2ll(vector<int>&arr)
+{
+    node *head=new node(arr[0],nullptr);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        node* temp=new node(arr[i],nullptr);
+        mover->next=temp;
+        mover=temp;
+        // or
+        // mover=mover->next;
+    }
+    return head;
+}
+int main()
+{
+   vector<int>arr={3,5,5,2};
+   node* y=new node(arr[0],nullptr);
+   cout<<y->data<<endl;
+
+   node *head=convertarr2ll(arr);
+   cout<<head->data<<endl;
+
+   // traversal in ll
+   node* temp=head;
+   while(temp)
+   {
+     cout<<temp->data<<" ";
+     temp=temp->next;
+   }
+}
