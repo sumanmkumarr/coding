84bfa22c553ff047ca65c384f973639fddb29a16
suman kumar <sumang91222@gmail.com>
2024-09-15 17:23:29 +0530

    delete_value_in_ll

--- linked_list/delete.cpp
+++ linked_list/delete.cpp
@@ -47,12 +47,25 @@ node* deletehead(node* head)
 
 
 // print element of linked list
+// void print(node* head)
+// {
+//     node* temp=head;
+//     while(temp!=NULL)
+//     {
+//         cout<<temp->data<<" ";
+//         temp=temp->next;
+//     }
+// }
+
+
+
+// print data of linked list
 void print(node* head)
 {
-    node* temp=head;
-    while(temp!=NULL)
+    node *temp=head;
+    while(temp !=NULL)
     {
-        cout<<temp->data<<" ";
+        cout<<temp->data;
         temp=temp->next;
     }
 }
@@ -78,9 +91,69 @@ node* deletetail(node* head)
 }
 
 
+
+// delete kth element
+node *deletek(node *head,int k)
+{
+    node* temp=head;
+    if(temp==NULL) return NULL;
+    if(k==1)
+    {
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    node *prev=NULL;
+    int c=0;
+    while(temp)
+    {
+       c=c+1;
+       if(c==k)
+       {
+          prev->next=prev->next->next;
+          free(temp);
+          break;
+       }
+       prev=temp;
+       temp=temp->next;
+    }
+    return head;
+}
+
+
+
+// delete specific value from list
+node *deletev(node *head,int val)
+{
+    node *temp=head;
+    if(temp==NULL)
+    {
+        return NULL;
+    }
+    if(head->data==val)
+    {
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    node *prev=NULL;
+    while(temp!=NULL)
+    {
+        if(temp->data==val)
+        {
+            prev->next=prev->next->next;
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    return head;
+}
+
 int main()
 {
-   vector<int>arr={8,3,2,9,3};
+   vector<int>arr={8,3,2,9,3,8,4,67};
    node* head=convertArr2LL(arr);
    cout<<head->data<<endl;
    print(head);
@@ -97,6 +170,23 @@ int main()
    // delete tail node
    node* head3=deletetail(head2);
    print(head3);
+   cout<<endl;
+
+
+   // delete kth element
+   int k;
+   cin>>k;
+   node *head4=deletek(head3,k);
+   print(head4);
+   cout<<endl;
+
+
+
+   // delete value
+   int val;
+   cin>>val;
+   node *head5=deletev(head4,val);
+   print(head5);
    cout<<endl;
    
 }
