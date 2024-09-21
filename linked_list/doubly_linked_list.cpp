c2e5501039adf2fbc067c022ee29ba77ce605d30
suman kumar <sumang91222@gmail.com>
2024-09-21 20:21:06 +0530

    DELETE_HEAD

--- linked_list/doubly_linked_list.cpp
+++ linked_list/doubly_linked_list.cpp
@@ -23,16 +23,54 @@ node *convertArr2DoubleLL(vector<int>&arr)
     for(int i=1;i<arr.size();i++)
     {
         node *temp=new node(arr[i],nullptr,nullptr);
-        mover->next=temp;
         temp->back=mover;
+        mover->next=temp;
         mover=mover->next;
     }
     return head;
 }
+
+
+
+// print all element of linked list
+void print(node *head)
+{
+    node *temp=head;
+    while(temp!=0)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+// delete the head of double linked list
+node *deleteh(node *head)
+{
+    node *temp=head;
+
+    if(temp == NULL || temp->next == NULL)
+    {
+        return NULL;
+    }
+    head=head->next;
+    head->back=nullptr;
+    temp->next=nullptr;
+    free(temp);
+
+    return head;
+}
 int main()
 {
-    vector<int>arr={4,9,6,2};
+    vector<int>arr={4,9,6,2,9,4,2};
     node *head=convertArr2DoubleLL(arr);
     cout<<head->next->back->next->data;
+    cout<<endl;
+    print(head);
+    cout<<endl;
+
+    // delete the head
+    head=deleteh(head);
+    print(head);
 
 }
