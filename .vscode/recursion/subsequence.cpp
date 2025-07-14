5bfd3812552ed6d7295ed61df594165a749b4f6a
suman kumar <sumang91222@gmail.com>
2025-07-14 19:35:11 +0530

    subsequence

--- .vscode/recursion/subsequence.cpp
+++ .vscode/recursion/subsequence.cpp
@@ -8,15 +8,19 @@
 // if 6 is there in any subsequence then it should be before 1 and after 2,4 only 
 
 // time complexity of subsequence is N(2^N)
+
+
+
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-void subsequence(int index,int arr[],vector<int> &brr,int n)
+void subsequence_print(int index,int arr[],vector<int> &brr,int n)
 {
-    if(index==n)
+    if(index == n)
     {
-        for(auto it :brr)
+        for(auto it:brr)
         {
             cout<<it<<" ";
         }
@@ -24,32 +28,94 @@ void subsequence(int index,int arr[],vector<int> &brr,int n)
         return;
     }
 
-    // this is pick condition
+    // pick element condition
     brr.push_back(arr[index]);
+    subsequence_print(index + 1,arr,brr,n);
 
-    subsequence(index+1,arr,brr,n);
-    
-
-    // this is not pick condition
+    // not pick element condition
     brr.pop_back();
-    subsequence(index+1,arr,brr,n);
-
-
+    subsequence_print(index + 1,arr,brr,n);
 }
 
 int main()
 {
     int n;
     cin>>n;
-
     int arr[n];
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
-
-    vector<int>brr;
-
-    subsequence(0,arr,brr,n);
+    vector<int> brr;
+    int index = 0;
+    subsequence_print(index,arr,brr,n);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void subsequence(int index,int arr[],vector<int> &brr,int n)
+// {
+//     if(index==n)
+//     {
+//         for(auto it :brr)
+//         {
+//             cout<<it<<" ";
+//         }
+//         cout<<endl;
+//         return;
+//     }
+
+//     // this is pick condition
+//     brr.push_back(arr[index]);
+
+//     subsequence(index+1,arr,brr,n);
+    
+
+//     // this is not pick condition
+//     brr.pop_back();
+//     subsequence(index+1,arr,brr,n);
+
+
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+//     }
+
+//     vector<int>brr;
+
+//     subsequence(0,arr,brr,n);
+
+//     return 0;
+
+// }
+
