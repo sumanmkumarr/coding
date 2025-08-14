8e6d98514d788f013520e34496df2c7cddfa241c
suman kumar <sumang91222@gmail.com>
2025-08-14 11:15:59 +0530

    subsequence

---
+++ 30_most_question/subsequence.cpp
@@ -0,0 +1,105 @@
+// subsequence  -- a contiguous and non contiguous sequence which follow the order
+
+// subarray -- a contiguous sequence which follow the order
+
+// follow the order means ,let's take example for getting this point
+// if arr ={2,4,6,1} then ,if 2 is there in any subsequence then priority of 2 is first only because it is at first index
+
+// if 6 is there in any subsequence then it should be before 1 and after 2,4 only 
+
+// time complexity of subsequence is N(2^N)
+
+
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsequence_print(int index,int arr[],vector<int> &brr,int n)
+{
+    if(index == n)
+    {
+        for(auto it:brr)
+        {
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    // pick element condition
+    brr.push_back(arr[index]);
+    subsequence_print(index + 1,arr,brr,n);
+
+    // not pick element condition
+    brr.pop_back();
+    subsequence_print(index + 1,arr,brr,n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    vector<int> brr;
+    int index = 0;
+    subsequence_print(index,arr,brr,n);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void subsequence_result(int index,vector<int> &arr,vector<int> &brr,int n)
+// {
+//     if(index == n)
+//     {
+//         for(auto it: brr)
+//         {
+//             cout<<it<<" ";
+//         }
+//         cout<<endl;
+//         return;
+//     }
+
+//     // pick condition
+//     brr.push_back(arr[index]);
+//     subsequence_result(index+1,arr,brr,n);
+
+//     // not pick condition
+//     brr.pop_back();
+//     subsequence_result(index+1,arr,brr,n);
+
+// }
+
+// int main()
+// {
+//     int n;
+//     vector<int> arr = {2,4,6,1};
+//     n = arr.size();
+//     vector<int> brr;
+//     subsequence_result(0,arr,brr,n);
+
+// }
